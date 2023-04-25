05a9895e2e097b1b28c4c09eff8f08b225fe80af
Deepu kumar <Deepu2946@gmail.com>
2023-04-25 18:14:24 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,26 +1,6 @@
-// import logo from "./logo.svg";
-// import './App.css';
-
+import FirstScreen from "./FirstScreen";
 function App() {
-  return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <h1>Name : Deepu Kumar</h1>
-    </div>
-  );
+  return <FirstScreen />;
 }
 
 export default App;
