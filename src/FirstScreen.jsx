b04c927e63e6bd2350ec18f8ddbec0afb34446b0
Deepu kumar <Deepu2946@gmail.com>
2023-04-25 19:11:58 +0530

    second

--- src/FirstScreen.jsx
+++ src/FirstScreen.jsx
@@ -1,21 +1,26 @@
 import React from "react";
-// import { NavLink } from "react-router-dom";
 import axios from "axios";
 
 const FirstScreen = () => {
   const [data, setData] = React.useState({ home: "", login: "" });
+  const [trigger, setTrigger] = React.useState(true);
   async function home() {
     axios.get("https://drdeepu.pythonanywhere.com/").then((res) => {
       setData({ ...data, home: res.data });
+      console.log(res);
     });
   }
   async function login() {
     axios.get("https://drdeepu.pythonanywhere.com/login").then((res) => {
       setData({ ...data, login: res.data });
+      console.log(res);
     });
   }
-  home();
-  login();
+  React.useEffect(() => {
+      login();
+    home();
+    setTrigger(false);
+  }, [trigger]);
   return (
     <>
       <div>Data is : {data.home}</div>
