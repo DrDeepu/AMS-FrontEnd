a5be86e8d8e358d24118a39edf4907f9ff2e67e3
Deepu kumar <Deepu2946@gmail.com>
2023-04-25 18:36:40 +0530

    second

--- src/FirstScreen.jsx
+++ src/FirstScreen.jsx
@@ -1,52 +1,26 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+// import { NavLink } from "react-router-dom";
+import axios from "axios";
 
 const FirstScreen = () => {
+  const [data, setData] = React.useState({ home: "", login: "" });
+  async function home() {
+    axios.get("https://drdeepu.pythonanywhere.com/").then((res) => {
+      setData({ ...data, home: res.data });
+    });
+  }
+  async function login() {
+    axios.get("https://drdeepu.pythonanywhere.com/login").then((res) => {
+      setData({ ...data, login: res.data });
+    });
+  }
+  home();
+  login();
   return (
-    <div id="intro" className="bg-image vh-100 shadow-1-strong">
-      <video
-        style={{ minWidth: "100%", minHeight: "100%" }}
-        // playsinline
-        autoPlay
-        muted
-        loop
-        type="video/mp4"
-      >
-        <source
-          className="h-100"
-          src="https://mdbootstrap.com/img/video/Lines.mp4"
-        />
-      </video>
-      <div
-        className="mask"
-        // style={{
-        //   background:
-        //     "linear-gradient( 45deg,rgba(29, 236, 197, 0.7),rgba(91, 14, 214, 0.7) 100%)",
-        // }}
-      >
-        <div className="container d-flex align-items-center justify-content-center text-center h-100">
-          <div className="text-white">
-            <h1 className="mb-3">Alumni</h1>
-            <h5 className="mb-4">Welcome to UTU Alumni Network</h5>
-            <NavLink
-              className="btn btn-outline-light btn-lg m-2"
-              to="#"
-              role="button"
-              rel="nofollow"
-            >
-              Sign Up
-            </NavLink>
-            <NavLink
-              className="btn btn-outline-light btn-lg m-2"
-              to="#"
-              role="button"
-            >
-              Sign In
-            </NavLink>
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+      <div>Data is : {data.home}</div>
+      <div>Login is : {data.login}</div>
+    </>
   );
 };
 
