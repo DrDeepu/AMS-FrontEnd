37a27d42631df2eee066a42d1cb3bd24fecaf585
Deepu kumar <Deepu2946@gmail.com>
2023-04-25 19:15:59 +0530

    second

--- src/FirstScreen.jsx
+++ src/FirstScreen.jsx
@@ -3,24 +3,25 @@ import axios from "axios";
 
 const FirstScreen = () => {
   const [data, setData] = React.useState({ home: "", login: "" });
-  const [trigger, setTrigger] = React.useState(true);
-  async function home() {
-    axios.get("https://drdeepu.pythonanywhere.com/").then((res) => {
-      setData({ ...data, home: res.data });
-      console.log(res);
-    });
-  }
-  async function login() {
-    axios.get("https://drdeepu.pythonanywhere.com/login").then((res) => {
-      setData({ ...data, login: res.data });
-      console.log(res);
-    });
-  }
+
   React.useEffect(() => {
-      login();
+    async function home() {
+      axios.get("https://drdeepu.pythonanywhere.com/").then((res) => {
+        setData({ ...data, home: res.data });
+        console.log(res);
+      });
+    }
+    async function login() {
+      axios.get("https://drdeepu.pythonanywhere.com/login").then((res) => {
+        setData({ ...data, login: res.data });
+        console.log(res);
+      });
+    }
+    login();
     home();
-    setTrigger(false);
-  }, [trigger]);
+    // setTrigger(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <>
       <div>Data is : {data.home}</div>
